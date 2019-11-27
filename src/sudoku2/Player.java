5046a58a0fd9853e153a0d1880d8947f45e6756d
dado_ <dado_@144.35.96.62>
2019-11-26 17:30:38 -0700

    Added player board

--- src/sudoku2/Player.java
+++ src/sudoku2/Player.java
@@ -1,14 +1,32 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package sudoku2;
 
-/**
- *
- * @author vivian
- */
+
 public class Player {
+    private String firstName;
+    private String lastName;
+    private int numOfTimesWon = 0;
+    private int numOfTimesLost = 0;
     
+    public Player(String firstName, String lastName)  {  
+        this.firstName = firstName;
+        this.lastName = lastName;
+        System.out.printf("Hello %s %s", firstName, lastName);
+    }
+    
+    public void addToWon(){
+        this.numOfTimesWon+=1;
+    }
+    
+    public void addToLost(){
+        this.numOfTimesLost+=1;
+    }
+    
+    public int getNumTimesWon(){
+        return this.numOfTimesWon;
+    }
+    
+    public int getNumTimesLost(){
+        return this.numOfTimesLost;
+    }
 }
